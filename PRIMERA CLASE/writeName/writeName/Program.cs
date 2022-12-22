47e98d6e5639e80f5e248879fdfd8c6e3b2f4c01
LO04 <luisortizcenteno@gmail.com>
2022-12-22 11:43:50 -0500

    first commit

---
+++ PRIMERA CLASE/writeName/writeName/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace writeName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cual es tu nombre: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Tu nombre es: "+name);
+            Console.WriteLine("La fecha actual es: "+ DateTime.Now.ToLongDateString());
+        }
+    }
+}
