e494a229501d2b78bb00f86d344c6c9661f5e342
LO04 <luisortizcenteno@gmail.com>
2022-12-28 18:35:10 -0500

    Thrid Commit

---
+++ TERCERA CLASE/TerceraClase-Struct/thridClass-Struct/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace thridClass_Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente("Nombre",999,"Direccion","email",true);
+            Console.WriteLine(cliente.NomCompleto);
+        }
+    }
+    public struct Cliente
+    {
+        
+        public string NomCompleto { get; }
+        public int Telefono { get;}
+        public string Direccion { get; }
+        public string Email { get;}
+        public bool ClienteNuevo { get; }
+        public Cliente(string nombre,int telefono,string direccion,string email,bool clientenuevo)
+        {
+            NomCompleto = nombre;
+            Telefono = telefono;
+            Direccion = direccion;
+            Email = email;
+            ClienteNuevo = clientenuevo;
+
+        }
+    }
+}
